1a73d0a4e4eb24ed06ce60a99fd7c70f1c135b36
Ruan Carlos <ruancarlosmeira@gmail.com>
2023-12-05 19:50:06 -0300

    Add initial project files and configurations

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="app">
+  <main class="container mx-auto">
+    <slot />
+  </main>
+</div>
+
+<style lang='postcss'>
+	:global(html){
+		background-color: theme(colors.red.100);
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	
+</script>
+
+<section>
+	<h1 class="text-6xl font-bold">
+		teste
+	</h1>
+</section>
+
+<style>
+	
+</style>
